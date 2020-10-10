184bc14ad703eb226c16a0b08eb9f0880486777b
Ayush khaskalam <ayushkhaskalam98@gmail.com>
2020-10-10 20:59:23 +0530

    Mini project related to time

--- src/index.js
+++ src/index.js
@@ -1,34 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-
-const name = 'Ayush';
-const img1 = "https://picsum.photos/id/1/367/269"
-const img2 = "https://picsum.photos/id/2/368/268"
-const img3 = "https://picsum.photos/id/3/369/265"
+import './index.css'
 
 
+let CurDate = new Date();
+CurDate = CurDate.getHours(2020, 10, 10, 20);
 
-const header = {
-  textAlign: 'center',
-  color: 'pink',
-  textTransform: 'capitalize',
-  fontWeight: '200',
-  textShadow: '0px 2px 4px white',
-  marginTop: '60px'
+let Greeting = '';
+const cssStyle = {};
+
+if (CurDate >= 1 && CurDate < 12) {
+  Greeting = 'Good Morning';
+  cssStyle.color = "red";
+} else if (CurDate >= 12 && CurDate < 19) {
+  Greeting = 'Good Afternoon';
+  cssStyle.color = "blue";
+} else {
+  Greeting = 'Goodnight';
+  cssStyle.color = "yellow";
 }
 
 ReactDOM.render(
   <>
-    <h1 className="heading" style={header}>My  name is {name}</h1>
-    
-    <div className="img">
-    <img src="https://picsum.photos/id/10/367/267" alt="img" />
-    <img src={img1}/>
-    <img src={img2}/>
-    <img src={img3}/>
-    </div>
-  </>
-  , document.getElementById('root')
-
-)
+  <div>
+<h1>Hello sir 
+ <span style={cssStyle}>  {Greeting} </span>
+ </h1>
+ </div>
+ </>, 
+ document.getElementById('root'))
