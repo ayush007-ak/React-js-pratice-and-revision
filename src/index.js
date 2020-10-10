778a0e0619acf9d3eddbd8db9baa788ae49f8bad
Ayush khaskalam <ayushkhaskalam98@gmail.com>
2020-10-10 12:34:23 +0530

    CSSs styling

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './index.css';
 
 const name = 'Ayush';
-const img1 = "https://picsum.photos/id/1/367/267"
+const img1 = "https://picsum.photos/id/1/367/269"
+const img2 = "https://picsum.photos/id/2/368/268"
+const img3 = "https://picsum.photos/id/3/369/265"
 ReactDOM.render(
   <>
-    <h1 contentEditable="true">My  name is {name}</h1>
+    <h1 className="heading">My  name is {name}</h1>
+    
+    <div className="img">
     <img src="https://picsum.photos/id/10/367/267" alt="img" />
     <img src={img1}/>
+    <img src={img2}/>
+    <img src={img3}/>
+    </div>
   </>
   , document.getElementById('root')
 
