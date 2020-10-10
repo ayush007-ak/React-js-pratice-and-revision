bfd3a447cafd2762df9c30c7fac9cedfbc903a31
Ayush khaskalam <ayushkhaskalam98@gmail.com>
2020-10-10 20:03:26 +0530

    CSS inline styling with js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,21 @@ const name = 'Ayush';
 const img1 = "https://picsum.photos/id/1/367/269"
 const img2 = "https://picsum.photos/id/2/368/268"
 const img3 = "https://picsum.photos/id/3/369/265"
+
+
+
+const header = {
+  textAlign: 'center',
+  color: 'pink',
+  textTransform: 'capitalize',
+  fontWeight: '200',
+  textShadow: '0px 2px 4px white',
+  marginTop: '60px'
+}
+
 ReactDOM.render(
   <>
-    <h1 className="heading">My  name is {name}</h1>
+    <h1 className="heading" style={header}>My  name is {name}</h1>
     
     <div className="img">
     <img src="https://picsum.photos/id/10/367/267" alt="img" />
