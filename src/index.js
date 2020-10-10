298dd7e257d5e9f80206d8a34cd9e34b49e51009
Ayush khaskalam <ayushkhaskalam98@gmail.com>
2020-10-10 10:44:15 +0530

    Template litrals

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import ReactDOM from 'react-dom';
 //we can only use Expression not Statement inside {curly braces}
 
 
-const name = 'ayush';
+const fname = 'ayush';
+const lname  = 'khaskalam';
+
+//console.log(`This is template litrals ${fname}`)
+
 ReactDOM.render(
 <>
-   <h1>My {name}</h1>
+   <h1> {`My name is ${fname } ${lname}`}</h1>
   <p> Mu licky {2+4}</p>
   <p> Mu licky {Math.random()}</p>  
   </>
