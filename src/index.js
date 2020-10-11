af3d4d31a7aba2f2a07458018983676626985153
Ayush khaskalam <ayushkhaskalam98@gmail.com>
2020-10-11 19:51:37 +0530

    Calci

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import App from './App';
+import {add , Sub , Multi , Divide} from './Calc';
 
 ReactDOM.render(
   <>
-   <App/>
+ <ul>
+   <li>{add(40 , 4)}</li>
+   <li>{Sub(40 , 4)}</li>
+   <li>{Multi(40 , 4)}</li>
+   <li>{Divide(40 , 4)}</li>
+ </ul>
   </>,
   document.getElementById('root')
 )
