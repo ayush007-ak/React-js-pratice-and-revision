df45e6af557b84ddea42776ca13a8864634147eb
Ayush khaskalam <ayushkhaskalam98@gmail.com>
2020-10-15 20:54:58 +0530

    map method done

--- src/index.js
+++ src/index.js
@@ -1,39 +1,50 @@
-const myarr = ['ayush', 'khas', 'kalam']
+// const myarr = ['ayush', 'khas', 'kalam']
 
-console.log(myarr[0])
-console.log(myarr[1])
-console.log(myarr[2])
-
-
-
-
-const newarr = myarr.map(function(cvalues , i){   //As a loop access kr skte ha array ko map method use krke
-  return cvalues + ' : '+ i;  //currentvalue , i=index
-})
-console.log(newarr)
+// console.log(myarr[0])
+// console.log(myarr[1])
+// console.log(myarr[2])
 
 
 
+
+// const newarr = myarr.map(function(cvalues , i){   //As a loop access kr skte ha array ko map method use krke
+//   return cvalues + ' : '+ i;  //currentvalue , i=index
+// })
+// console.log(newarr)
+
+
+
+<p id='showdata'></p>//to show in html
 
 //EXAMPLE-
 const StudentData = [
   {
     id: 1,
-    name: ayush,
-    lastname: khaskalam
+    name: 'ayush',
+    lastname: 'khaskalam'
 
   },
   {
     id:2,
-    name: Trisha,
-    lastname:sur
+    name: 'Trisha',
+    lastname:'sur'
   },
   {
     id:3,
-    name:aditi,
-    lastname: khaskalam
+    name:'aditi',
+    lastname: 'khaskalam'
   },
-]
+];
+
+
+const maparr = StudentData.map( (cvalue) => {
+  
+return `my name is ${cvalue.name}. my last name is ${cvalue.lastname}`
+} )
+
+console.log(maparr)
+document.getElementById('showdata').innerHTML=maparr;
+ 
 
 
 
